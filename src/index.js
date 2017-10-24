bd4647e197add77eadf75206cd2238fe2a14d60e
lucask42 <lucasbkellner@gmail.com>
2017-10-24 10:14:03 -0700

    its alive!

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Box extends React.component {
+class Box extends React.Component {
   selectBox = () => {
     this.props.selectBox(this.props.row, this.props.col)
   }
-  
+
   render() {
     return (
       <div
@@ -20,12 +20,12 @@ class Box extends React.component {
 
 class Grid extends React.Component {
   render() {
-    const width = this.props.cols * 14;
+    const width = this.props.cols * 16;
     var rowsArr = [];
     var boxClass = "";
 
-    for (var i = 0, i < this.props.rows, i++) {
-      for (var j = 0, j < this.props.cols, j++) {
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
         let boxId = i + "_" + j;
         boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
         rowsArr.push(
@@ -37,23 +37,31 @@ class Grid extends React.Component {
             col={j}
             selectBox={this.props.selectBox}
           />
-        )
+        );
       }
     }
 
     return (
       <div className="grid" style={{ width: width }}>
-        {{ rowsArr }}
+        { rowsArr }
       </div>
     );
   }
 }
 
+// class Buttons extends React.Component{
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
+
 class Main extends React.Component {
   constructor() {
     super();
-    this.speed = 100;
-    this.rows = 30;
+    this.speed = 25;
+    this.rows = 50;
     this.cols = 50;
 
     this.state = {
@@ -61,15 +69,91 @@ class Main extends React.Component {
       gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
     }
   }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy,
+    })
+  }
+
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4) === 1.0) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy
+    });
+  }
+
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  }
+
+  play = () => {
+    const g = this.state.gridFull;
+    const g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i +  1][j - 1]) count++;
+        if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+
+
+
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1,
+    })
+  }
+
+  componentDidMount() {
+    this.seed();
+    this.playButton();
+  }
+
   render() {
     return (
       <div>
         <h1>The Game of Life</h1>
+        {
+        // <Buttons
+        //   playButton={this.playButton}
+        //   pauseButton={this.pauseButton}
+        //   slow={this.slow}
+        //   fast={this.fast}
+        //   clear={this.clear}
+        //   seed={this.seed}
+        //   gridSize={this.gridSize}
+        // />
+        }
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
           cols={this.cols}
-          selectBox=(this.selectBox)
+          selectBox={this.selectBox}
         />
         <h2>Generations: {this.state.generations}</h2>
       </div>
@@ -77,6 +161,8 @@ class Main extends React.Component {
   }
 }
 
-
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
+}
 
 ReactDOM.render(<Main />, document.getElementById('root'));
